7cd449d4b696945085501d0fdcfe999cdcf70ee5
AleksandrBracyuk <aleksandr.bracyuk@abcsolutions.pro>
2019-09-08 17:44:11 +0300

    reinsert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { AComponent } from './a/a.component';
+import { A1Component } from './a1/a1.component';
+import { A2Component } from './a2/a2.component';
+import { A21Component } from './a21/a21.component';
+import { A22Component } from './a22/a22.component';
+import { A23Component } from './a23/a23.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    AComponent,
+    A1Component,
+    A2Component,
+    A21Component,
+    A22Component,
+    A23Component
   ],
   imports: [
+    NgbModule,
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AComponent]
 })
 export class AppModule { }
